f5b1d496f780ba56307f5df82c5d4820d8d3dcc5
josemam <jmlosvillares@correo.ugr.es>
2015-04-14 13:59:49 +0200

    Comentarios

--- midi.cpp
+++ midi.cpp
@@ -5,10 +5,13 @@ using namespace std;
 // Genera el trozo de datos MIDI que almacena los sonidos
 bool GetChunk(const Voice v[], char data[], unsigned int &chpos, int INPUT_LIMIT)
 {
+  /* Se leen simultáneamente las cuatro voces, añadiendo cuando es necesario
+      caracteres que indican final o inicio de sonido
+  */
   chpos = 0;
   unsigned short int pos[] = {0,0,0,0};
 
-  unsigned short int nothingfor = 0;
+  unsigned short int nothingfor = 0;  // Semicorcheas en las que no ha habido cierre de sonidos
   
   unsigned short int countdown[] = {0,0,0,0};
   bool finish[] = {false, false, false, false};
@@ -62,14 +65,18 @@ bool Write(const char* name, const char* data, unsigned int chpos)
   ofstream f(name, ios::out|ios::binary);
   if (f)
   {
+    // Se escribe la cabecera
     char header[52] = {0x4d, 0x54, 0x68, 0x64, 0x00, 0x00, 0x00, 0x06, 0x00, 0x01, 0x00, 0x02, 0x04, 0x00, 0x4d, 0x54, 0x72, 0x6b, 0x00, 0x00, 0x00, 0x1a, 0x00, 0xff, 0x58, 0x04, 0x04, 0x02, 0x18, 0x08, 0x00, 0xff, 0x59, 0x02, 0x00, 0x00, 0x00, 0xff, 0x51, 0x03, 0x07, 0xa1, 0x20, 0xa0, 0x00, 0xff, 0x2f, 0x00, 0x4d, 0x54, 0x72, 0x6b};
     f.write(header, 52);
 
+    // Se calcula la longitud del trozo y se escribe en hexadecimal
     unsigned int length = chpos + 12;
     for (short int x = 3; x > -1; x--)
       f << (char) ((length >> 8*x)%256);    
 
-    char middle[8] = {0x00, 0xff, 0x03, 0x01, 0xc7, 0x00, 0xc0, 0x34};
+    // Se escribe el trozo y el final del fichero
+    char instrument = 0x34; // Coro Aahs (52 en decimal)
+    char middle[8] = {0x00, 0xff, 0x03, 0x01, 0xc7, 0x00, 0xc0, instrument};
     f.write(middle, 8);
 
     f.write(data, chpos);
